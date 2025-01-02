a845b66879f5e20e377520cc3d437ff9f3a42722
Dominic Lakshan <dominiclakshank@gmail.com>
2025-01-03 00:17:18 +0530

    refactor login tests to remove unnecessary steps and improve clarity

--- tests/cypress/e2e/tests/ui/login/login.cy.js
+++ tests/cypress/e2e/tests/ui/login/login.cy.js
@@ -1,17 +1,14 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import LoginPage from "../../../page-objects/loginPage.cy";
 
-// Navigate to the login page
 Given("I am on the login page", () => {
   LoginPage.visit();
 });
 
-// Enter username in the login form
 When("I enter username with {string}", (username) => {
   LoginPage.enterUserName(username);
 });
 
-// Enter password in the login form
 When("I enter password with {string}", (password) => {
   LoginPage.enterPassword(password);
 });
@@ -24,7 +21,6 @@ Then("I should see the home page", () => {
   LoginPage.seeHomePage();
 });
 
-// Validate the presence of an error message
 Then("I should see error message with {string}", (error) => {
   LoginPage.getErrorMessage(error);
 });
