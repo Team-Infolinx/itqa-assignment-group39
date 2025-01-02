8059b769719c137f84f61bf1097f5f710f115256
Nimesh Nuwanga <ymnimesh@gmail.com>
2025-01-02 13:59:49 +0530

    update config file & .json

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,17 +1,32 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const addCucumberPreprocessorPlugin =
-  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const { allureCypress } = require("allure-cypress/reporter");
+
+async function setupNodeEvents(on, config) {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    })
+  );
+
+  allureCypress(on, config);
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
-    async setupNodeEvents(on, config) {
-      const bundler = createBundler();
-      on("file:preprocessor", bundler);
-      await addCucumberPreprocessorPlugin(on, config);
-      return config;
-    },
-    specPattern: "cypress/e2e/**/*.feature",
-    baseUrl: "http://localhost:3000",
+    specPattern: "**/*.feature",
+    setupNodeEvents,
+    baseUrlUI: "http://localhost:3000",
+    baseUrlAPI: "http://localhost:7081",
+    chromeWebSecurity: false,
   },
 });
